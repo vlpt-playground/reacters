f5ac63d7f774b6b86ab7588af04ba900614abcb0
velopert <public.velopert@gmail.com>
2019-03-15 17:31:48 +0900

    Initial commit

--- reacters-client/src/App.js
+++ reacters-client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import loadable from '@loadable/component';
 import Core from './containers/base/Core';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
 
-const PostListPage = loadable(() => import('./pages/PostListPage'));
-const PostPage = loadable(() => import('./pages/PostPage'));
-const LoginPage = loadable(() => import('./pages/LoginPage'));
-const RegisterPage = loadable(() => import('./pages/RegisterPage'));
-const WritePage = loadable(() => import('./pages/WritePage'));
+// const PostListPage = loadable(() => import('./pages/PostListPage'));
+// const PostPage = loadable(() => import('./pages/PostPage'));
+// const LoginPage = loadable(() => import('./pages/LoginPage'));
+// const RegisterPage = loadable(() => import('./pages/RegisterPage'));
+// const WritePage = loadable(() => import('./pages/WritePage'));
 
 const App = () => {
   return (
